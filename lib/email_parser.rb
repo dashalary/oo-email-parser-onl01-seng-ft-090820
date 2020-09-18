470bde5bbf8d80217a4cb445f5994518eae143f7
Dasha Lary <dk2800@nyu.edu>
2020-09-18 18:25:00 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,9 +13,7 @@ end
 def parse
 
 email_array = email_addresses.split(/\s/)
-if email_addresses.include?(",")
-  email_addresses.delete (",")
-end
+
 
 email_addresses = email_array[0]
 email_array
